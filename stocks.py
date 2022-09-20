0f028941211d1e72510b9d3f261942a85cdc52f5
Pablo M. Rivera <pablomrivera1976@gmail.com>
2022-09-19 20:35:31 -0400

    stock purchases and snippets

--- stocks.py
+++ stocks.py
@@ -8,21 +8,38 @@ def stock_purchases():
 
     # Given the prices above and a client's investment budget, how much stock can they buy?
     # 1.1 TODO: Ask the client's name (use the string: "What is your name? ") and save it into a variable
+    Client_name = input("What is your name? ")
 
     # 1.2 TODO: Ask the client how many dollars they would like to invest (use the string: "How much would you like to invest? $")
     # and save it into a variable
     # NOTE: When you use the `input` function to get user input, what do numbers get saved as?
+    dollars_invest = int(input("How much would you like to invest? $"))
 
     # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
     # NOTE: Take a look at how this input string prints out
-    # stock_name = input("\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ")
+    stock_name = input("\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ")
     
     # Now that you have all of the client info, you can check how much they can purchase of the stock
     # they're interested in.
 
     # 1.4 TODO: Use `if/elif/else` conditional logic to determine how much stock the client can buy,
     # and save it in a variable
+    purchase_power = 0
+    if stock_name == 'Amazon':
+        purchase_power = amazon
+    elif stock_name == "Apple":
+        purchase_power = apple
+    elif stock_name == "FB":
+        purchase_power = fb
+    elif stock_name == "Google":
+        purchase_power = google
+    elif stock_name == "Microsoft":
+        purchase_power = msft
+    
+    number_stocks = dollars_invest // purchase_power
 
     # 1.5 TODO: Once you've calculated the number of stocks that can be purchased,
     # Use an f-string to print the result for the client, ala:
+    print(f'{Client_name} has ${dollars_invest} to invest and can buy {number_stocks} of {stock_name} at the current price of ${purchase_power}.')
     # Alex has $5000 to invest and can buy 50 shares of Apple at the current price of $100.
+stock_purchases()
